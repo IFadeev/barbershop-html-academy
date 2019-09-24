a98c3a0ca1cb52a4e4e35db5798a582cf50b497a
Fadeev Ivan <githubfadeev@gmail.com>
2019-09-24 21:57:00 +0300

    Show and hidden modal-form for loggin in site

--- js/common.js
+++ js/common.js
@@ -1,15 +1,34 @@
 window.addEventListener('load', function() {
-  let loginLink = document.getElementById('login-link-id');
-  let modalLogin = document.getElementById('modal-login-id');
-  let modalClose = document.getElementById('modal-close-pop-id');
-  let loginInput = modalLogin.querySelector('[name=login]');
+  const loginLink = document.getElementById('login-link-id');
+  const modalLogin = document.getElementById('modal-login-id');
+  const modalClose = document.getElementById('modal-close-pop-id');
+  const loginInput = modalLogin.querySelector('[name=login]');
+  const passInput = modalLogin.querySelector('[name=password]');
+  const storage = localStorage.getItem('login');
+  const loginForm = document.querySelector('.login-form');
   
 
   loginLink.addEventListener('click', function(e) {
     e.preventDefault();
     modalLogin.classList.toggle('modal-show');
     loginInput.focus();
+    if (storage) {
+      loginInput.value = storage;
+    }
 
+    loginForm.addEventListener('submit', function(e){
+      if (!loginInput.value || !passInput.value) {
+        e.preventDefault();
+        alert('Нужно ввести и логин и пароль!');
+      }
+      else {
+        localStorage.setItem('login', loginInput.value);
+        e.preventDefault();
+        console.log(localStorage.getItem('login'))
+      };
+    });
+
+    
     modalClose.addEventListener('click', function(e) {
       e.preventDefault();
       modalLogin.classList.remove('modal-show');
